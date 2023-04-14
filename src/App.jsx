b9b1dd0025986c08915f909a890d7f9a6113e51b
berbolot <berbolotd@gmail.com>
2023-04-14 19:52:16 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import CountPage from './pages/CountPage'
+
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+ <CountPage/>
+
+
+    </div>
+  )
+}
+
+export default App
